704da3f9fe590acb1a60faba91dce977da2d76c2
Ayana Bando <ayanaflax@yahoo.com>
2018-09-13 16:33:09 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :birthdate, :string, :grade, :integer
+    add_column :birthdate, :grade, :string
   end
 end
